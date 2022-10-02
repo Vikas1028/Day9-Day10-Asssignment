4c4a5ceecd81a04c78265938c96ce8d416937410
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-10-02 10:05:26 +0530

    Display Welcome Message

---
+++ AddressBookManagement.java
@@ -0,0 +1,9 @@
+package AddressBookManagementSystem;
+
+public class AddressBookManagement {
+
+	public static void main(String[] args) {
+		// print welcome message
+		System.out.println("Welcome to Address Book Program");
+	}
+}
